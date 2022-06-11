9604d7394edbb2d9e3cbbf915d5001e8b0929aee
Jando <alexandroguterez@gmail.com>
2022-06-11 14:45:19 -0700

    Make folder system work. Local saving doesn't work.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './style.css';
 import plus from './plus-circle.png';
+import { compareAsc, format } from 'date-fns'
 
-const todoNode = (title, description, dueDate, priority) => {
+const todo = (title, description, dueDate, priority) => {
     const getTitle = () => title;
     const getDescription = () => description;
     const getDueDate = () => dueDate;
@@ -15,20 +16,71 @@ const todoNode = (title, description, dueDate, priority) => {
         const container = document.getElementById('projects');
         const newDiv = document.createElement('div');
         newDiv.innerHTML = project;
+        newDiv.addEventListener('click', ()=> {
+            currentProject = project;
+            renderTasks(project);
+        });
         container.appendChild(newDiv);
      }
      const renderTask = (task) => {
-         console.log('nothing');
+         const container = document.getElementById('tasks');
+         const newDiv = document.createElement('div');
+         newDiv.classList.add('task');
+         const taskTitle = document.createElement('div');
+         taskTitle.innerHTML = task.getTitle();
+         newDiv.appendChild(taskTitle);
+         const taskDate = document.createElement('div');
+         taskDate.innerHTML = task.getDueDate();
+         newDiv.appendChild(taskDate);
+         newDiv.addEventListener('click', ()=> {
+            //generate the details and append to body
+        const details = document.createElement('div');
+        details.classList.add('form-container');
+        const detHead = document.createElement('h1');
+        detHead.innerHTML = task.getTitle();
+        details.appendChild(detHead);
+        const detDesc = document.createElement('div');
+        detDesc.innerHTML = task.getDescription();
+        details.appendChild(detDesc);
+        document.body.appendChild(details);
+        const detDue = document.createElement('div');
+        //add amount of days left in paranthesis
+        detDue.innerHTML = `Due: ${task.getDueDate()} (In blank days)`;
+        details.appendChild(detDue);
+        overlay.classList.remove('hidden');
+         });
+         container.appendChild(newDiv);
      }
-     return {renderProj, renderTask};
+     const renderTasks = (project) => {
+        const container = document.getElementById('tasks');
+            //remove existing tasks, making sure not to remove button
+            while(container.childElementCount > 1) {
+                container.removeChild(container.lastChild);
+            }
+            //add tasks to DOM
+            for(const task in projectsList[project]) {
+                domStuff.renderTask(projectsList[project][task]);
+            }
+     }
+     return {renderProj, renderTask, renderTasks};
  })();
 
 let projectsList = {};
+if(window.localStorage.length) {
+    let str = localStorage.getItem('data');
+    console.log(JSON.parse(str))
+}
 
 //if user has no data, populate a new project
+if(!Object.keys(projectsList).length) {
+    projectsList['My Project'] = [todo('my task', 'do cool stuff', 'tomorrow', 'urgent'), 
+    todo('my other task', 'do more cool stuff', 'tomorrow', 'urgent')];
+    
+    projectsList['My other Project'] = [todo('my otro task', 'do cool stuff', 'tomorrow', 'urgent'), 
+    todo('my other otro task', 'do more cool stuff', 'tomorrow', 'urgent')];
+}
 
-projectsList['My Project'] = [];
-
+let currentProject = Object.keys(projectsList)[0];
 /*Code for generating skeleton of page */
 const content = document.getElementById('content');
 
@@ -42,13 +94,6 @@ mainTitle.innerHTML = 'TodoList';
 const projects = document.createElement('div');
 projects.setAttribute('id', 'projects');
 content.appendChild(projects);
-
-//populate projects
-for (const project in projectsList) {
-    domStuff.renderProj(project);
-    //populate tasks in this project
-
-};
 
 const projBtnContainer = document.createElement('div');
 const projBtn = new Image();
@@ -69,24 +114,66 @@ taskBtn.setAttribute('id', 'taskBtn');
 taskBtnContainer.appendChild(taskBtn);
 tasks.appendChild(taskBtnContainer);
 
+//populate projects
+for (const project in projectsList) {
+    domStuff.renderProj(project);
+};
+domStuff.renderTasks(currentProject);
+
+//add project popup
 const projForm = document.createElement('form');
-projForm.setAttribute('id', 'form-container');
+projForm.classList.add('form-container');
+projForm.setAttribute('id', 'projForm');
 projForm.classList.add('hidden');
 const projFormHead = document.createElement('h1');
 projFormHead.innerHTML = 'Add a new project';
 projForm.appendChild(projFormHead);
 const projNameField = document.createElement('input');
 projNameField.setAttribute('type', 'text');
-projNameField.setAttribute('id', 'projName');
 projNameField.setAttribute('placeholder', 'project name');
 projForm.appendChild(projNameField);
 const submitProj = document.createElement('input');
 submitProj.setAttribute('type', 'button');
 submitProj.setAttribute('value', 'Create Project');
-submitProj.setAttribute('id', 'subProj');
 projForm.appendChild(submitProj);
-
 document.body.appendChild(projForm);
+
+//add task popup
+const taskForm = document.createElement('form');
+taskForm.classList.add('form-container');
+taskForm.setAttribute('id', 'taskForm');
+taskForm.classList.add('hidden');
+const taskFormHead = document.createElement('h1');
+taskFormHead.innerHTML = 'Add a new Task';
+taskForm.appendChild(taskFormHead);
+const taskNameField = document.createElement('input');
+taskNameField.setAttribute('type', 'text');
+taskNameField.setAttribute('placeholder', 'Task name');
+taskForm.appendChild(taskNameField);
+const taskDeskField = document.createElement('input');
+taskDeskField.setAttribute('type', 'text');
+taskDeskField.setAttribute('placeholder', 'Task name');
+taskForm.appendChild(taskNameField);
+const taskDescField = document.createElement('textarea');
+taskDescField.setAttribute('placeholder', 'Description (optional)');
+taskForm.appendChild(taskDescField);
+const taskDateField = document.createElement('input');
+taskDateField.setAttribute('type', 'date');
+taskForm.appendChild(taskDateField);
+const urgencyLabel = document.createElement('label');
+urgencyLabel.setAttribute('for', 'urgency');
+urgencyLabel.innerHTML = 'urgency';
+taskForm.appendChild(urgencyLabel);
+const taskUrgencyField = document.createElement('input');
+taskUrgencyField.setAttribute('type', 'range');
+taskUrgencyField.setAttribute('value', 'urgency');
+taskForm.appendChild(taskUrgencyField);
+const submitTask = document.createElement('input');
+submitTask.setAttribute('type', 'button');
+submitTask.setAttribute('value', 'Create Task');
+taskForm.appendChild(submitTask);
+document.body.appendChild(taskForm);
+
 const overlay = document.createElement('div');
 overlay.setAttribute('id', 'overlay');
 overlay.classList.add('hidden');
@@ -96,18 +183,49 @@ projBtn.addEventListener('click', () => {
         projForm.classList.remove('hidden');
         overlay.classList.remove('hidden');
     });
+taskBtn.addEventListener('click', ()=> {
+        taskForm.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+});
 
-document.getElementById('overlay').addEventListener('click', ()=> {
+overlay.addEventListener('click', ()=> {
     projForm.classList.add('hidden');
     overlay.classList.add('hidden');
+    taskForm.classList.add('hidden');
+    if(document.body.lastChild != overlay) {
+        //this removes tasks since they are generated when clicked (so many tasks)
+        document.body.removeChild(document.body.lastChild);
+    }
     });
-
-document.getElementById('subProj').addEventListener('click', ()=> {
-    if(document.getElementById('form-container').checkValidity()) {
+submitProj.addEventListener('click', ()=> {
+    if(document.getElementById('projForm').checkValidity()) {
         projectsList[projNameField.value] = [];
         domStuff.renderProj(projNameField.value);
-        projNameField.value = ''
+        projNameField.value = '';
         projForm.classList.add('hidden');
         overlay.classList.add('hidden');
     }
     });
+submitTask.addEventListener('click', ()=> {
+    if(document.getElementById('taskForm').checkValidity()) {
+       projectsList[currentProject].push(todo(
+        taskNameField.value,
+        taskDescField.value,
+        taskDateField.value,
+        taskUrgencyField.value
+       ));
+        domStuff.renderTask(projectsList[currentProject][projectsList[currentProject].length - 1]);
+        taskNameField.value = '';
+        taskDescField.value = '';
+        taskDateField.value = '';
+        taskUrgencyField.value = '';
+        taskForm.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+    });
+    console.log(projectsList);
+    console.log(JSON.parse(JSON.stringify(projectsList['My Project'][0])));
+    window.onbeforeunload = ()=> {
+        localStorage.clear();
+        localStoreage.setItem('data', JSON.stringify(projectsList));
+    }
